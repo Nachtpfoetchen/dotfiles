623e57f8d05e8c4675acca1eaba3a8453f5e064f
nachtpfoetchen <47748053+Nachtpfoetchen@users.noreply.github.com>
2022-02-06 20:51:21 +0100

    Update i3pystatusconf.py

--- i3pystatusconf.py
+++ i3pystatusconf.py
@@ -3,9 +3,6 @@ from i3pystatus.weather import wunderground
 
 status = Status()
 
-# Displays clock like this:
-# Tue 30 Jul 11:59:46 PM KW31
-#                          ^-- calendar week
 status.register("clock",
     format="%a %-d %b %H:%M",
     hints = {'markup': 'pango'},
@@ -26,17 +23,6 @@ status.register("keyboard_locks",
 status.register("temp",
     format="{temp:.0f}°C",)
 
-# The battery monitor has many formatting options, see README for details
-
-# This would look like this, when discharging (or charging)
-# ↓14.22W 56.15% [77.81%] 2h:41m
-# And like this if full:
-# =14.22W 100.0% [91.21%]
-#
-# This would also display a desktop notification (via D-Bus) if the percentage
-# goes below 5 percent while discharging. The block will also color RED.
-# If you don't have a desktop notification demon yet, take a look at dunst:
-#   http://www.knopwob.org/dunst/
 status.register("battery",
     format="[{glyph} ]{percentage_design:.0f}%[{status}]",
     alert=True,
@@ -59,36 +45,6 @@ status.register("battery",
         ],
     )
 
-# This would look like this:
-# Discharging 6h:51m
-#status.register("battery",
-#    format="{status} {remaining:%E%hh:%Mm}",
-#    alert=True,
-#    alert_percentage=5,
-#    status={
-#        "DIS":  "Discharging",
-#        "CHR":  "Charging",
-#        "FULL": "Bat full",
-#    },
-#    )
-
-# Displays whether a DHCP client is running
-#status.register("runwatch",
-#    name="DHCP",
-#    path="/var/run/dhclient*.pid",)
-
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
-#status.register("network",
-#    interface="eth0",
-#    format_up="{v4cidr}",)
-
-# Note: requires both netifaces and basiciw (for essid and quality)
 status.register("network",
     interface="wlp3s0",
     format_up="{essid} {quality}",
@@ -97,14 +53,6 @@ status.register("network",
     color_up = "#9edc60",
     color_down = "#f81141",
     ) 
-
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
-#status.register("disk",
-#    path="/",
-#    format="{used}/{total}G [{avail}G]",)
-
 status.register(
     "pulseaudio",
     color_unmuted = "#9edc60",
@@ -114,18 +62,6 @@ status.register(
     vertical_bar_width=1,
     vertical_bar_glyphs=["  ", " ", ""],
 )
-
-
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-status.register("mpd",
-    format="{title}{status}{album}",
-    status={
-        "pause": "▷",
-        "play": "▶",
-        "stop": "◾",
-    },)
 
 status.register("shell",
     format="{output}",
